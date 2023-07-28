b1a0dd0a51b60f6b13960e0d3244f4d32e9f95ba
Sandra <pogonsa@gmail.com>
2023-07-28 21:40:31 +0200

    v 0.8.0.2

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -6,8 +6,10 @@ import {
 
 const main = document.getElementById('main');
 const cbox = document.querySelectorAll('.nav-item > .nav-item-link');
+const cboxHamburger = document.querySelectorAll('.nav-item-hamburger > .nav-item-hamburger-link');
 
 const hWrap = document.querySelector('.header-container');
+hWrap.style.background = "";
 
 for (let i = 0; i < cbox.length; i++) {
     cbox[i].addEventListener("click", (ev) => {
@@ -15,7 +17,7 @@ for (let i = 0; i < cbox.length; i++) {
         ev.preventDefault(); // Esto hace que el link no cargue la URL a la que apunta
         const linkHref = ev.currentTarget.href; // Almacenamos la URL en una constante
 
-        hWrap.style.background = "";
+
         if (linkHref.includes('/home'))
             main.innerHTML = homeHtml;
         else if (linkHref.includes('/experiencias'))
@@ -23,6 +25,25 @@ for (let i = 0; i < cbox.length; i++) {
         else if (linkHref.includes('/proyectos')) {
             main.innerHTML = projectsHtml;
             hWrap.style.background = 'linear-gradient(to right, #1e1e1e 50%, #08bdbd 50%';
+        }
+
+    });
+}
+
+for (let i = 0; i < cboxHamburger.length; i++) {
+    cboxHamburger[i].addEventListener("click", (ev) => {
+
+        ev.preventDefault(); // Esto hace que el link no cargue la URL a la que apunta
+        const linkHref = ev.currentTarget.href; // Almacenamos la URL en una constante
+
+
+        if (linkHref.includes('/home'))
+            main.innerHTML = homeHtml;
+        else if (linkHref.includes('/experiencias')) {
+            main.innerHTML = experienceHtml;
+
+        } else if (linkHref.includes('/proyectos')) {
+            main.innerHTML = projectsHtml;
         }
 
     });
