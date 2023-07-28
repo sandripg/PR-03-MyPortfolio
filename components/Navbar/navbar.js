fe32a0d4f55af980231e2dafcd70ae5d305f2ca6
Sandra <pogonsa@gmail.com>
2023-07-28 21:11:59 +0200

    v 0.8.0.1

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -4,10 +4,10 @@ import {
     projectsHtml
 } from "../../constants.js"
 
-
-
 const main = document.getElementById('main');
 const cbox = document.querySelectorAll('.nav-item > .nav-item-link');
+
+const hWrap = document.querySelector('.header-container');
 
 for (let i = 0; i < cbox.length; i++) {
     cbox[i].addEventListener("click", (ev) => {
@@ -15,12 +15,15 @@ for (let i = 0; i < cbox.length; i++) {
         ev.preventDefault(); // Esto hace que el link no cargue la URL a la que apunta
         const linkHref = ev.currentTarget.href; // Almacenamos la URL en una constante
 
+        hWrap.style.background = "";
         if (linkHref.includes('/home'))
             main.innerHTML = homeHtml;
         else if (linkHref.includes('/experiencias'))
             main.innerHTML = experienceHtml;
-        else if (linkHref.includes('/proyectos'))
+        else if (linkHref.includes('/proyectos')) {
             main.innerHTML = projectsHtml;
+            hWrap.style.background = 'linear-gradient(to right, #1e1e1e 50%, #08bdbd 50%';
+        }
 
     });
 }
