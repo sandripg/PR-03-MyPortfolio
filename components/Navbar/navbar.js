c1375ea2c133ae01e18b210878ec68ab9c542b43
Sandra <pogonsa@gmail.com>
2023-07-30 15:31:59 +0200

    v.0.8.4

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -2,7 +2,14 @@ import {
     homeHtml,
     experienceHtml,
     projectsHtml
-} from "../../constants.js"
+} from "../../constants.js";
+
+import {
+    init
+}
+from "../ProjectCard/ProjectCard.js"
+
+
 
 const main = document.getElementById('main');
 const cbox = document.querySelectorAll('.nav-item > .nav-item-link');
@@ -10,20 +17,23 @@ const cboxHamburger = document.querySelectorAll('.nav-item-hamburger > .nav-item
 
 const hWrap = document.querySelector('.header-container');
 hWrap.style.background = "";
+main.innerHTML = homeHtml;
+
+
 
 for (let i = 0; i < cbox.length; i++) {
     cbox[i].addEventListener("click", (ev) => {
-
         ev.preventDefault(); // Esto hace que el link no cargue la URL a la que apunta
         const linkHref = ev.currentTarget.href; // Almacenamos la URL en una constante
         hWrap.style.background = "";
+
         if (linkHref.includes('/home'))
             main.innerHTML = homeHtml;
         else if (linkHref.includes('/experiencias'))
             main.innerHTML = experienceHtml;
         else if (linkHref.includes('/proyectos')) {
-            main.innerHTML = projectsHtml;
-            hWrap.style.background = 'linear-gradient(to right, #1e1e1e 50%, #08bdbd 50%';
+            main.innerHTML = init();
+            // hWrap.style.background = 'linear-gradient(to right, #1e1e1e 50%, #08bdbd 50%';
         }
 
     });
@@ -40,7 +50,7 @@ for (let i = 0; i < cboxHamburger.length; i++) {
         else if (linkHref.includes('/experiencias')) {
             main.innerHTML = experienceHtml;
         } else if (linkHref.includes('/proyectos')) {
-            main.innerHTML = projectsHtml;
+            main.innerHTML = init();
         }
 
     });
