51e677b5fecd66b89306c00eae566b40dbf38d3d
Sandra <pogonsa@gmail.com>
2023-07-30 19:03:05 +0200

    test

--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -1,65 +1,65 @@
  const projects = [{
-         title: 'Tienda Estática',
-         status: 'Despliegue ✅',
-         description: 'Tienda inventada de ventas de Perfumes',
-         tech: ['HTML 5', 'CSS 3'],
-         github: 'https://github.com/sandripg/PR-01-Web-Responsive',
-         link: 'https://moonarome.netlify.app/',
-         img: '../public/moonarome_static.jpg'
-     }, {
-         title: 'Tienda Dinámica',
-         status: 'Despliegue ✅',
-         description: 'Tienda Dinámica con filtro de búsqueda',
-         tech: ['HTML 5', 'CSS 3', 'JS'],
-         github: 'https://github.com/sandripg/PR-02-Dynamic-Web',
-         link: 'https://moonarome-dynamic.netlify.app',
-         img: './public/moonarome_dinamic.jpg'
-     },
+     title: 'Tienda Estática',
+     status: 'Despliegue ✅',
+     description: 'Tienda inventada de ventas de Perfumes',
+     tech: ['HTML 5', 'CSS 3'],
+     github: 'https://github.com/sandripg/PR-01-Web-Responsive',
+     link: 'https://moonarome.netlify.app/',
+     img: '../public/moonarome_static.jpg'
+   }, {
+     title: 'Tienda Dinámica',
+     status: 'Despliegue ✅',
+     description: 'Tienda Dinámica con filtro de búsqueda',
+     tech: ['HTML 5', 'CSS 3', 'JS'],
+     github: 'https://github.com/sandripg/PR-02-Dynamic-Web',
+     link: 'https://moonarome-dynamic.netlify.app',
+     img: './public/moonarome_dinamic.jpg'
+   },
 
-     {
-         title: 'Color Flipper',
-         status: 'Despliegue ✅',
-         description: 'Aplicación dinámica cambios de color',
-         tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS'],
-         github: 'https://github.com/sandripg/Color-Flipper-San',
-         link: 'https://color-flipper-san.netlify.app',
-         img: './public/colorflipper.jpg'
-     },
+   {
+     title: 'Color Flipper',
+     status: 'Despliegue ✅',
+     description: 'Aplicación dinámica cambios de color',
+     tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS'],
+     github: 'https://github.com/sandripg/Color-Flipper-San',
+     link: 'https://color-flipper-san.netlify.app',
+     img: './public/colorflipper.jpg'
+   },
 
-     {
-         title: 'Rick & Morty',
-         status: 'Despliegue ✅',
-         description: 'Api Rest Rick & Morty',
-         tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS', 'Api Rest'],
-         github: 'https://github.com/sandripg/rickandmorty',
-         link: 'https://rick-and-morty-san.netlify.app',
-         img: './public/rickandmorty.jpg'
-     },
+   {
+     title: 'Rick & Morty',
+     status: 'Despliegue ✅',
+     description: 'Api Rest Rick & Morty',
+     tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS', 'Api Rest'],
+     github: 'https://github.com/sandripg/rickandmorty',
+     link: 'https://rick-and-morty-san.netlify.app',
+     img: './public/rickandmorty.jpg'
+   },
 
-     {
-         title: 'Currículum Dinámico',
-         status: 'Despliegue ✅',
-         description: 'Currículum Dinámico con Skills',
-         tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS', 'Boostrap'],
-         github: 'https://github.com/sandripg/sandracv',
-         link: 'https://sandra-pompido-cv.netlify.app/',
-         img: './public/cv.jpg'
-     },
+   {
+     title: 'Currículum Dinámico',
+     status: 'Despliegue ✅',
+     description: 'Currículum Dinámico con Skills',
+     tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS', 'Boostrap'],
+     github: 'https://github.com/sandripg/sandracv',
+     link: 'https://sandra-pompido-cv.netlify.app/',
+     img: './public/cv.jpg'
+   },
 
-     {
-         title: 'Portfolio',
-         status: 'Despliegue ✅',
-         description: 'Portfolio Personal de trabajo Dinámico',
-         tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS'],
-         github: 'https://github.com/sandripg/PR-03-MyPortfolio',
-         link: 'https://portfoliosandrapompido.netlify.app/',
-         img: './public/portfolio.jpg'
-     },
+   {
+     title: 'Portfolio',
+     status: 'Despliegue ✅',
+     description: 'Portfolio Personal de trabajo Dinámico',
+     tech: ['HTML 5', 'CSS 3', 'JS', 'Node JS'],
+     github: 'https://github.com/sandripg/PR-03-MyPortfolio',
+     link: 'https://portfoliosandrapompido.netlify.app/',
+     img: './public/portfolio.jpg'
+   },
  ];
 
 
  const projectMainHtml = (projects) => {
-     return `<section class="projects-container">
+   return `<section class="projects-container">
         <div id="text-float-projects" class="text-focus-in">
           <div class="text-name-container">
             <h2 class="text-name-projects">3. Proyectos Realizados</h2>
@@ -70,8 +70,8 @@
  };
 
  const projectHtml = (project) => {
-     var techs = getTechHtml(project.tech);
-     return `<div class="projects">
+   var techs = getTechHtml(project.tech);
+   return `<div class="projects">
            <div class="project-element-content">
              <h3 class="project-title">${project.title}</h3>
              <p class="project-status">${project.status}</p>
@@ -95,25 +95,27 @@
                <img src="${project.img}" alt="">
              </div>
            </div>
+
+
          </div>`
  };
 
  const getTechHtml = (techs) => {
-     var result = '';
-     for (let index = 0; index < techs.length; index++) {
-         const tech = techs[index];
-         result += '<li>' + tech + '</li>'
-     }
-     return result;
+   var result = '';
+   for (let index = 0; index < techs.length; index++) {
+     const tech = techs[index];
+     result += '<li>' + tech + '</li>'
+   }
+   return result;
  };
 
  export function init() {
-     var result = '';
+   var result = '';
 
-     for (let index = 0; index < projects.length; index++) {
-         const project = projects[index];
-         result += projectHtml(project);
-     }
+   for (let index = 0; index < projects.length; index++) {
+     const project = projects[index];
+     result += projectHtml(project);
+   }
 
-     return projectMainHtml(result);;
+   return projectMainHtml(result);;
  }
